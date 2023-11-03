60f5910ecd3b5ff9a8bb25b95cdc2bcbedace070
quizitocristiano10@gmail.com <quizitocristiano10@gmail.com>
2023-11-03 08:02:56 -0300

    criando uma página de categoria

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ var swiper = new Swiper(".home", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+
+const menu = document.querySelector('#menu-icon');
+const navebar = document.querySelector('.navbar');
+
+menu.addEventListener('click', function() {
+  // event.defaultPrevented();
+
+ menu.classList.toggle('bx-x');
+ navebar.classList.toggle('active');
+})
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navebar.classList.remove('active');
+}
