41ce9b0755928b8f215dea25ee4dd36f66b64547
quizitocristiano <quizitocristiano10@gmail.com>
2023-11-17 16:53:12 -0300

    css

--- script.js
+++ script.js
@@ -140,13 +140,12 @@ const carrinho = [];
 // Função para adicionar um produto ao carrinho
 function adicionarAoCarrinho(productIndex, isVegetable = false) {
   const produtosArray = isVegetable ? produtoLegumes : produtos;
-  const tagCarinho = document.querySelector('.counter')
+  const tagCarinho = document.querySelector(".counter");
   const produto = produtosArray[productIndex];
   carrinho.push({ ...produto, quantidade: 1, isVegetable });
   atualizarCarrinho();
   calcularTotal();
-  tagCarinho.innerHTML = carrinho.length &&
-    `${carrinho.length}`;
+  tagCarinho.innerHTML = carrinho.length && `${carrinho.length}`;
 }
 
 function atualizarCarrinho() {
@@ -208,21 +207,17 @@ produtos.forEach((produto, index) => {
     </div>
   `;
   container.innerHTML += productCardHTML;
-
-
 });
 
 // Função para trocar o ícone do coração;
 function trocarIconDeFrutas(el) {
-  if (el.classList.contains('bx-heart')) {
-    el.classList.remove('bx-heart');
-    el.classList.add('bxs-heart');
+  if (el.classList.contains("bx-heart")) {
+    el.classList.remove("bx-heart");
+    el.classList.add("bxs-heart");
   } else {
-    el.classList.remove('bxs-heart');
-    el.classList.add('bx-heart');
+    el.classList.remove("bxs-heart");
+    el.classList.add("bx-heart");
   }
-
-
 }
 
 produtoLegumes.forEach((myVegetableProducts, index) => {
@@ -231,8 +226,8 @@ produtoLegumes.forEach((myVegetableProducts, index) => {
       <img src="${myVegetableProducts.img}" alt="" />
       <h2>${myVegetableProducts.nome}</h2>
       <h3 class="price">R$: ${myVegetableProducts.preco.toFixed(
-    2
-  )} <span>kg</span></h3>
+        2
+      )} <span>kg</span></h3>
       <i class="bx bx-cart-alt" onclick="adicionarAoCarrinho(${index}, true)"></i>
       <i class="bx bx-heart" onclick="trocarIcone(this)"></i>
      
@@ -243,162 +238,235 @@ produtoLegumes.forEach((myVegetableProducts, index) => {
 
 // Função para trocar o ícone do coração
 function trocarIcone(element) {
-  if (element.classList.contains('bx-heart')) {
-    element.classList.remove('bx-heart');
-    element.classList.add('bxs-heart');
+  if (element.classList.contains("bx-heart")) {
+    element.classList.remove("bx-heart");
+    element.classList.add("bxs-heart");
   } else {
-    element.classList.remove('bxs-heart');
-    element.classList.add('bx-heart');
+    element.classList.remove("bxs-heart");
+    element.classList.add("bx-heart");
   }
 }
 
 // por que os clientes nos amam?
 
-
-document.addEventListener('DOMContentLoaded', function () {
-  var openModalBtn = document.getElementById('openModalBtn');
-  var modal = document.getElementById('myModal');
-  var fecharModalClicado = document.getElementById('fechar-modal');
+document.addEventListener("DOMContentLoaded", function () {
+  var openModalBtn = document.getElementById("openModalBtn");
+  var modal = document.getElementById("myModal");
+  var fecharModalClicado = document.getElementById("fechar-modal");
 
   // Abrir o modal ao clicar no botão
-  openModalBtn.addEventListener('click', function () {
-    modal.style.display = 'block';
+  openModalBtn.addEventListener("click", function () {
+    modal.style.display = "block";
   });
 
-  fecharModalClicado.addEventListener('click', function (e) {
-    modal.style.display = 'none';
-  })
+  fecharModalClicado.addEventListener("click", function (e) {
+    modal.style.display = "none";
+  });
   // Fechar o modal ao clicar no botão de fechar ou fora dele
-  window.addEventListener('click', function (event) {
+  window.addEventListener("click", function (event) {
     if (event.target == modal) {
-      modal.style.display = 'none';
+      modal.style.display = "none";
     }
   });
-
 });
 
+function validarNomeCompleto() {
+  const nomeCompletoInput = document.getElementById("nomeCompleto");
+  const nomeCompleto = nomeCompletoInput.value.trim();
 
-
-
-
-
-
-function validarCliente() {
-  // Obtendo os valores dos campos
-  const bloco = document.getElementById('bloco').value;
-  const andar = document.getElementById('andar').value;
-  const apartamento = document.getElementById('apartamento').value;
-  const NomeElemento = document.querySelector('#Nome');
-
-  if (NomeElemento.value === '') {
-    NomeElemento.style.border = '1px solid red'; 
+  if (nomeCompleto === "") {
+    nomeCompletoInput.classList.add("campo-vazio");
+  } else {
+    nomeCompletoInput.classList.remove("campo-vazio");
   }
-  // Validando os campos (exemplo simples)
-  if (bloco.trim() === '' && andar.trim() === '' && apartamento.trim() === '') {
-    // Adiciona uma classe CSS para alterar a cor dos campos
-    document.getElementById('bloco').classList.add('campo-vazio');
-    document.getElementById('andar').classList.add('campo-vazio');
-    document.getElementById('apartamento').classList.add('campo-vazio');
+}
 
-    alert('Por favor, preencha pelo menos um campo.');
-    return;
+function validarNomeCompleto() {
+  const nomeCompletoInput = document.getElementById("nomeCompleto");
+  const nomeCompleto = nomeCompletoInput.value.trim();
+
+  // Verifique se o campo está vazio ou não contém apenas letras
+  if (nomeCompleto === "" || !/^[a-zA-Z\s]+$/.test(nomeCompleto)) {
+    nomeCompletoInput.classList.add("campo-vazio");
+    alert("Por favor, insira um nome válido.");
+  } else {
+    nomeCompletoInput.classList.remove("campo-vazio");
+  }
+}
+
+function validarCamposDeBloco() {
+  const bloco = document.getElementById("bloco");
+  const andar = document.getElementById("andar");
+  const apartamento = document.getElementById("apartamento");
+
+  // Remove as classes existentes
+  bloco.classList.remove("campo-vazio", "campo-preenchido");
+  andar.classList.remove("campo-vazio", "campo-preenchido");
+  apartamento.classList.remove("campo-vazio", "campo-preenchido");
+
+  const valorBloco = bloco.value.trim();
+  const valorAndar = andar.value.trim();
+  const valorApartamento = apartamento.value.trim();
+
+  // Verifica se pelo menos um dos campos foi preenchido
+  if (valorBloco !== "" || valorAndar !== "" || valorApartamento !== "") {
+    // Se pelo menos um campo foi preenchido, adiciona a classe campo-preenchido a todos os campos
+    bloco.classList.add("campo-preenchido");
+    andar.classList.add("campo-preenchido");
+    apartamento.classList.add("campo-preenchido");
+  } else {
+    // Se nenhum campo foi preenchido, adiciona a classe campo-vazio a todos os campos
+    bloco.classList.add("campo-vazio");
+    andar.classList.add("campo-vazio");
+    apartamento.classList.add("campo-vazio");
+
+    alert("Por favor, preencha pelo menos um campo.");
+  }
+}
+
+function validarCelular() {
+  const celularInput = document.getElementById("Celular");
+  const celular = celularInput.value.trim();
+  let valor = telefoneInput.value.replace(/\D/g, '');
+
+  // Padrão esperado: (XX) XXXXX-XXXX ou XX XXXXX-XXXX
+  if (valor.length >= 2) {
+    valor = `(+${valor.slice(0, 2)}) ${valor.slice(2)}`;
   }
 
-
- 
-  // Adicione aqui sua lógica de validação adicional, se necessário
-
-  // Se pelo menos um campo estiver preenchido, faça algo, como enviar os dados para o servidor
-  alert('Cliente validado com sucesso!');
-  // Aqui você pode adicionar a lógica para enviar os dados para o servidor ou realizar outras ações
+  if (valor.length >= 11) {
+    valor = `${valor.slice(0, 10)}-${valor.slice(10)}`;
+  }
 }
 
 
 
 
 
+function validarCPF(cpf) {
+  // Remove caracteres não numéricos
+  cpf = cpf.replace(/\D/g, "");
+
+  // Verifica se o CPF tem 11 dígitos
+  if (cpf.length !== 11) {
+    return false;
+  }
+
+  // Verifica se todos os dígitos são iguais, o que torna o CPF inválido
+  if (/^(\d)\1{10}$/.test(cpf)) {
+    return false;
+  }
+
+  // Calcula os dígitos verificadores
+  let soma = 0;
+  for (let i = 0; i < 9; i++) {
+    soma += parseInt(cpf.charAt(i)) * (10 - i);
+  }
+  let resto = soma % 11;
+  let digito1 = resto < 2 ? 0 : 11 - resto;
+
+  soma = 0;
+  for (let i = 0; i < 10; i++) {
+    soma += parseInt(cpf.charAt(i)) * (11 - i);
+  }
+  resto = soma % 11;
+  let digito2 = resto < 2 ? 0 : 11 - resto;
+
+  // Verifica se os dígitos calculados são iguais aos dígitos do CPF
+  return (
+    digito1 === parseInt(cpf.charAt(9)) && digito2 === parseInt(cpf.charAt(10))
+  );
+}
+
+function validarCliente() {
+  const cpfInput = document.getElementById("cpf");
+  const cpf = cpfInput.value.trim();
+
+  validarNomeCompleto();
+   
+  validarCelular();
+  validarCamposDeBloco();
+  if (!validarCPF(cpf)) {
+    cpfInput.classList.add("campo-vazio");
+    alert("CPF inválido. Por favor, verifique.");
+    return;
+  }
 
 
+  
+
+  // alert("CPF válido! Continue com a finalização da compra.");
 
 
-
+}
 
 // Customer Review Elements
 const dadosDosClientes = [
   {
-    nome: 'Quizito Cristiano',
+    nome: "Quizito Cristiano",
     customerImage: "./image/sol.jpg",
     message: `
     A Delicacty sempre me surpreende com a qualidade impecável dos
     produtos. Os legumes são frescos e as frutas são deliciosas. Além
     disso, a entrega rápida é um grande diferencial. Mal posso esperar
     para fazer meu próximo pedido!
-    `
-
+    `,
   },
   {
-    nome: 'Rafael T.',
+    nome: "Rafael T.",
     customerImage: "./image/quizito2.jpeg",
     message: `
     A Delicacty é a minha salvação quando estou sem tempo para ir ao
     supermercado. Eles entregam tão rapidamente que nunca fico sem
     frutas e legumes frescos em casa. E os descontos são uma grande
     ajuda para o orçamento.
-    `
-
+    `,
   },
 
   {
-    nome: 'Sandro',
+    nome: "Sandro",
     customerImage: "./image/mamae.jpeg",
     message: `
     Os produtos da Delicacty são tão frescos que parece que acabei de
     colhê-los eu mesmo. A entrega rápida é um grande benefício,
     especialmente quando estou ocupado. Os descontos também são muito
     apreciados!
-    `
-
+    `,
   },
 
   {
-    nome: 'Pedro Henrique',
+    nome: "Pedro Henrique",
     customerImage: "./image/nascerd-o-sol.jpg",
     message: `
     Nunca encontrei uma loja online que entregasse tão rapidamente
     quanto a Delicacty. Além disso, os produtos são de alta qualidade, e
     os descontos regulares são um bônus adicional que economiza meu
     dinheiro.
-    `
-
+    `,
   },
 
   {
-    nome: 'Mariana Dos Santos',
+    nome: "Mariana Dos Santos",
     customerImage: "./image/Quizito.jpeg",
     message: `
     A Delicacty sempre me surpreende com a qualidade impecável dos
     produtos. Os legumes são frescos e as frutas são deliciosas. Além
     disso, a entrega rápida é um grande diferencial. Mal posso esperar
     para fazer meu próximo pedido!
-    `
-
+    `,
   },
   {
-    nome: 'Temótio Luiz',
+    nome: "Temótio Luiz",
     customerImage: "./image/temotio.jpeg",
     message: `
     A Delicacty é a minha opção número um para alimentos frescos. A
     qualidade é impecável, a entrega é super rápida, e os descontos
     frequentes me fazem voltar sempre. Uma loja incrível!
-    `
-
+    `,
   },
+];
 
-
-
-]
-
-const myCustomers = document.querySelector('.customers-container');
+const myCustomers = document.querySelector(".customers-container");
 
 dadosDosClientes.map((cardDosCliente) => {
   const newDiv = document.createElement("div");
@@ -495,9 +563,7 @@ const dadosDosCartoes = [
     quantidade: "10 Item",
     backgroundColor: "#dee2ff",
   },
-
 ];
-
 
 const cartoesContainer = document.getElementById("categoria-container");
 
@@ -515,28 +581,6 @@ dadosDosCartoes.map((cartao) => {
   cartoesContainer.appendChild(div);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Função para abrir o modal
 function finalizar(e) {
   abrirNovoModal.classList.add("showNewModal");
@@ -548,8 +592,8 @@ function fecharModal() {
 }
 
 // Event listener para controlar cliques
-document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('finalizar-compra')) {
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("finalizar-compra")) {
     finalizar(e); // Abrir o modal
   } else if (e.target.classList.contains("bx-x")) {
     fecharModal(); // Fechar o modal
@@ -558,8 +602,3 @@ document.addEventListener('click', (e) => {
     fecharModal(); // Fechar o modal após enviar
   }
 });
-
-
-
-
-
